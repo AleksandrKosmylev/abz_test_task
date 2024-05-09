891e72285d360b4a6af0ff0fde2269fd79e9d507
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-05-09 12:38:42 +0300

    add update user option

--- employees/urls.py
+++ employees/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from employees.views import index, show_employees
+from employees.views import index, show_employees, EmployeeUpdateView
 
 urlpatterns = [
     path('', index, name="main"),
     path('employees/', show_employees, name='employees'),
+    path('<int:pk>/update/', EmployeeUpdateView.as_view(), name='employees_update'),
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import Http404
 from django.shortcuts import render
 from django.db.models import Q
-
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView
 
 from .models import Employees
-from .forms import SimpleForm
+from .forms import SimpleForm, EmployeesForm
 
 
 def index(request):
@@ -17,7 +19,7 @@ def is_valid_queryparam(param):
     return param != '' and param is not None
 
 
-def filter_employees(request):
+def filter_employee(request):
     # qs = Employees.objects.all().order_by('id')
     qs = Employees.objects.all()
     name_contains_query = request.GET.get('name_contains')
@@ -28,22 +30,22 @@ def filter_employees(request):
     salary_max = request.GET.get('salary_max')
 
     if is_valid_queryparam(name_contains_query):
-        qs = qs.filter_employees(name__icontains=name_contains_query)
+        qs = qs.filter(name__icontains=name_contains_query)
 
     if is_valid_queryparam(employment_position_query):
-        qs = qs.filter_employees(Q(employment_position__icontains=employment_position_query))
+        qs = qs.filter(Q(employment_position__icontains=employment_position_query))
 
     if is_valid_queryparam(date_min):
-        qs = qs.filter_employees(start_date__gte=date_min)
+        qs = qs.filter(start_date__gte=date_min)
 
     if is_valid_queryparam(date_max):
-        qs = qs.filter_employees(start_date__lt=date_max)
+        qs = qs.filter(start_date__lt=date_max)
 
     if is_valid_queryparam(salary_min):
-        qs = qs.filter_employees(salary__gte=salary_min)
+        qs = qs.filter(salary__gte=salary_min)
 
     if is_valid_queryparam(salary_max):
-        qs = qs.filter_employees(salary__lt=salary_max)
+        qs = qs.filter(salary__lt=salary_max)
 
     return qs
 
@@ -58,11 +60,11 @@ def show_employees(request):
         if form.is_valid():
             choice = list(form.cleaned_data.values())[0]
             context = {'form': form}
-            qs = filter_employees(request).order_by(choice)
+            qs = filter_employee(request).order_by(choice)
         else:
             raise Http404
     else:
-        qs = filter_employees(request).order_by('id')
+        qs = filter_employee(request).order_by('id')
 
     current_page = Paginator(list(qs), 50)
     page = request.GET.get('page')
@@ -74,3 +76,15 @@ def show_employees(request):
         context['queryset'] = current_page.page(current_page.num_pages)
 
     return render(request, "employees_list.html", context)
+
+
+class EmployeeUpdateView(LoginRequiredMixin, UpdateView):
+    model = Employees
+    form_class = EmployeesForm
+    login_url = reverse_lazy('/users/login/')
+    extra_context = {
+        'title': 'Edit employee',
+        'btn_text': 'Update',
+        'btn_class': 'btn-primary'}
+    template_name = 'edit_form.html'
+    success_url = reverse_lazy('employees')
