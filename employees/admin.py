bc531ae8bfeac84f6f24da832ad638f5a61121e7
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-05-09 22:07:00 +0300

    add model Employees to admin.py

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Employees
 
-# Register your models here.
+
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent', 'employment_position', 'salary', 'start_date')
+    # list_display = [field.name for field in Employees._meta.get_fields()]
+
+admin.site.register(Employees, SettingAdmin)
--- employees/models.py
+++ employees/models.py
@@ -8,6 +8,7 @@ class Employees(MPTTModel):
     employment_position = models.CharField(max_length=200, verbose_name='Position')
     salary = models.IntegerField(verbose_name='Salary')
     start_date = models.DateField(auto_now_add=False, verbose_name='Start_date')
+    #employment_photo = models.ImageField(blank=True, upload_to='images/%Y/%m/%d', help_text='150x150px', verbose_name='employment_photo')
 
     def __str__(self):
         return self.name
