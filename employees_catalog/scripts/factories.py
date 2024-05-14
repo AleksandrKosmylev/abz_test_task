423e0e055ec5fc4d468d8826a449df235dd339f7
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-05-14 18:07:07 +0300

    check db

--- employees_catalog/scripts/factories.py
+++ employees_catalog/scripts/factories.py
@@ -7,7 +7,7 @@ import random
 from employees_catalog.settings import MEDIA_ROOT, BASE_DIR
 
 fake = Faker(['ru_RU', 'en_US', 'de_DE', 'it_IT', 'fr_FR'])
-Faker.seed(1234)
+Faker.seed(4321)
 
 
 def run(*args):
@@ -50,33 +50,33 @@ def run(*args):
         deputy_head_of_depertment = []
         group_head = []
         chief_specialist = []
-        for tier_1 in range(1):
+        for tier_1 in range(2):
             f_date = fake.date_between_dates(date_start=datetime(2000, 1, 1), date_end=datetime(2023, 12,  31))
             name = fake.name()
             head_of_depertment.append(name)
             Employees.objects.create(name=name, employment_position=positions[0], salary=salaries[0], start_date=f_date)
-        for tier_2 in range(2):
+        for tier_2 in range(4):
             f_date = fake.date_between_dates(date_start=datetime(2000, 1, 1), date_end=datetime(2023, 12, 31))
             name = fake.name()
             deputy_head_of_depertment.append(name)
             random_parent = random.choice(head_of_depertment)
             parent = Employees.objects.get(name=random_parent)
             Employees.objects.create(name=name, parent=parent, employment_position=positions[1], salary=salaries[1], start_date=f_date)
-        for tier_3 in range(3):
+        for tier_3 in range(8):
             f_date = fake.date_between_dates(date_start=datetime(2000, 1, 1), date_end=datetime(2023, 12, 31))
             name = fake.name()
             group_head.append(name)
             random_parent = random.choice(deputy_head_of_depertment)
             parent = Employees.objects.get(name=random_parent)
             Employees.objects.create(name=name, parent=parent, employment_position=positions[2], salary=salaries[2], start_date=f_date)
-        for tier_4 in range(4):
+        for tier_4 in range(16):
             f_date = fake.date_between_dates(date_start=datetime(2000, 1, 1), date_end=datetime(2023, 12, 31))
             name = fake.name()
             chief_specialist.append(name)
             random_parent = random.choice(group_head)
             parent = Employees.objects.get(name=random_parent)
             Employees.objects.create(name=name, parent=parent, employment_position=positions[3], salary=salaries[3], start_date=f_date)
-        for tier_5 in range(5):
+        for tier_5 in range(50000):
             f_date = fake.date_between_dates(date_start=datetime(2000, 1, 1), date_end=datetime(2023, 12, 31))
             name = fake.name()
             random_parent = random.choice(chief_specialist)
